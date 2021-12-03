f62e5f8c3cf85f089c1a8882bae7142cc462cc60
Mariya Kartseva <kjojoj@rambler.ru>
2021-12-03 14:22:48 +0300

    feat(game.js): new feat

--- game.js
+++ game.js
@@ -5,26 +5,38 @@ const isNumber = function (num) {
 }
 
 function game() {
-    const number = Math.floor(100 * Math.random());
-    console.log(number);
-    return function guess() {
+    let number = Math.floor(100 * Math.random());
+    return function guess(i) {
+        console.log(number + " загаданное");
+        console.log("номер " + i);
         const numberUser = prompt("Угадай число от 1 до 100");
         if (!numberUser) {
             alert("Игра окончена");
         } else if (!isNumber(numberUser)) {
             alert("Введи число");
-            guess();
-        } else if (numberUser == number) { alert("Поздравляю, вы угадали!") }
-        else if (numberUser > number) {
-            alert("Загаданное число меньше");
-            guess();
-        } else if (numberUser < number) {
-            alert("Загаданное число больше");
-            guess();
+            guess(i);
+        } else {
+
+            i--;
+            if (i) {
+                if (numberUser == number) { alert("Поздравляю, вы угадали!") }
+                else if (numberUser > number) {
+                    alert("Загаданное число меньше, осталось " + i + " попытки");
+                    guess(i);
+                } else if (numberUser < number) {
+                    alert("Загаданное число больше, осталось " + i + " попытки");
+                    guess(i);
+                }
+            }
+            else
+                if (confirm("Попытки закончились, хотите сыграть еще?")) {
+                    game();
+                    guessNumber(10);
+                }
         }
     }
 }
 
 const guessNumber = game();
 
-guessNumber();
+guessNumber(10);
